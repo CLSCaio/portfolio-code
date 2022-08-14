853499dd3e9471625ec008f3d83e18bdf69c900c
Caio Sousa <caiodelimack@hotmail.com>
2022-08-14 09:23:56 -0300

    refactor: arrumando exp profissional

--- src/Views/ExpProfessional/content.js
+++ src/Views/ExpProfessional/content.js
@@ -1,28 +1,30 @@
 const description = ['Empresa: ', 'Cargo: ', 'Tempo de serviço: '];
 const activities = [
-  'Desenvolvimento de Aplicação Web (React, Redux, JavaScript, HTML e CSS)',
-  'Desenvolvimento de Aplicação Mobile (React Native, Redux e JavaScript)'
+  'React.js, JavaScript, HTML, CSS',
+  'Desenvolvimento de Aplicação Mobile (React Native, JavaScript, HTML, CSS)',
 ]
+
+const desen = 'Desenvolvimento de Aplicação Web ('
 
 export const formations = [
   {
     description,
     title: ['Pixter', 'Desenvolvedor FrontEnd', '03/22 até o momento'],
-    activities: [activities[0]],
+    activities: [`${desen}${[activities[0]]}, Typescript, Storybook)`],
   },
   {
     description,
     title: ['IBM', 'Desenvolvedor FrontEnd', '04/21 até 11/21'],
-    activities,
+    activities: [`${desen}${[activities[0]]}, Typescript, Redux)`],
   },
   {
     description,
     title: ['OSAS', 'Desenvolvedor FrontEnd', '09/20 até 03/21'],
-    activities: [...activities, 'Desenvolvimento de app mobile TruckerPay com React-Native']
+    activities: [`${desen}${[activities[0]]})`, activities[1], 'Desenvolvimento de app mobile TruckerPay com React-Native']
   },
   {
     description,
     title: ['Workana', 'Desenvolvedor FrontEnd - Freelancer', '01/20 até 09/20'],
-    activities: [...activities, 'Resolução de bugs']
+    activities: [`${desen}${[activities[0]]}, Next.js)`, 'Resolução de bugs']
   },
 ]
